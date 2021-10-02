0417637f7285a459d639ab37731c8ce0eb6857b8
anon <>
2021-10-02 14:03:36 -0700

    bounce and bite feel okay

--- scripts/EnemyDog.gd
+++ scripts/EnemyDog.gd
@@ -25,6 +25,6 @@ func _physics_process(delta):
 	if(dist > 400 && move_time > 0.4):
 		cmds.append("ranged")
 		
-	doInput(-1,delta)
+	#doInput(-1,delta)
 	applyForces(delta)
 	velocity = move_and_slide(velocity, Vector2.DOWN)
--- scripts/GenericDog.gd
+++ scripts/GenericDog.gd
@@ -23,44 +23,61 @@ var last_inputs = []
 var last_l_depress = 0
 var last_r_depress = 0
 
+var damagefx = preload("res://Hit.tscn")
+
 # consts
-var GRAVITY = 1000
-var MOVESPEED = 2500
-var DAMPEN_FACTOR = 10
+const GRAVITY = 1000
+const MOVESPEED = 2500
+const DAMPEN_FACTOR = 10
 
-var CLOSE_ATTACK_LAUNCH = 0.5
-var CLOSE_ATTACK_COOLDOWN = 0.7
-var CLOSE_ATTACK_VEL_BOOST = 700
-var CLOSE_ATTACK_RANGE = 200
-var CLOSE_ATTACK_DAMAGE = 20
+const CLOSE_ATTACK_HIT = 0.4
+const CLOSE_ATTACK_LAUNCH = 0.5
+const CLOSE_ATTACK_COOLDOWN = 0.7
+const CLOSE_ATTACK_VEL_BOOST = 700
+const CLOSE_ATTACK_RANGE = 120
+const CLOSE_ATTACK_DAMAGE = 10
+const CLOSE_ATTACK_KNOCKBACK = 800
 
-var RANGED_CHARGE_ONE = 0.5
-var RANGED_CHARGE_TWO = 1.0
-var RANGED_COOLDOWN = 0.5
-var RANGED_LAUNCH = 0.3
-var RANGED_ATTACK_DAMAGE = 15
+const POUNCE_ATTACK_HIT = 0.2
+const POUNCE_ATTACK_LAUNCH = 0.5
+const POUNCE_ATTACK_COOLDOWN = 1.7
+const POUNCE_ATTACK_VEL_BOOSTX = 3000
+const POUNCE_ATTACK_VEL_BOOSTY = -300
+const POUNCE_ATTACK_RANGE = 160
+const POUNCE_ATTACK_DAMAGE = 20
+const POUNCE_ATTACK_KNOCKBACK = 1200
 
-var FRISBEE_SPEED_X = 300
-var FRISBEE_SPEED_Y = 150
+const RANGED_CHARGE_ONE = 0.5
+const RANGED_CHARGE_TWO = 1.0
+const RANGED_COOLDOWN = 0.5
+const RANGED_LAUNCH = 0.3
+const RANGED_ATTACK_DAMAGE = 15
 
-var BALL_SPEED_X = 200
-var BALL_SPEED_Y = 400
+const FRISBEE_SPEED_X = 300
+const FRISBEE_SPEED_Y = 150
 
-var BACKUP_SPEED_RATIO = 0.7
+const BALL_SPEED_X = 200
+const BALL_SPEED_Y = 400
 
-var DASH_DETECTION_TIME = 0.1
-var DASH_VELOCITY = 700
-var DASH_COOLDOWN = 0.3
-var DASH_COST = 3
+const BACKUP_SPEED_RATIO = 0.7
 
-func hit(rev):
+const DASH_DETECTION_TIME = 0.1
+const DASH_VELOCITY = 1200
+const DASH_COOLDOWN = 0.3
+const DASH_COST = 3
+
+func hit(rev, dmg, knockback):
 	velocity.y = -200
-	velocity.x = rev * 2500
-	hp -= CLOSE_ATTACK_DAMAGE
+	velocity.x = rev * knockback
+	hp -= dmg
 	
 func catch():
 	hp -= RANGED_ATTACK_DAMAGE
 	# launch catch animation.
+
+
+func _ready():
+	setAnim("Idle",1)
 
 func checkForDash(rev):
 		# check for dash
@@ -71,20 +88,40 @@ func checkForDash(rev):
 		last_l_depress = time_elapsed
 		
 	if("right" in cmds and time_elapsed - last_r_depress < DASH_DETECTION_TIME):
+		attack_executed = "dash"
 		cooldown = DASH_COOLDOWN
 		hp -= DASH_COST
 		velocity.x += DASH_VELOCITY * rev
 		
 	if("left" in cmds and time_elapsed - last_l_depress < DASH_DETECTION_TIME):
+		attack_executed = "dash"
 		cooldown = DASH_COOLDOWN
 		hp -= DASH_COST
 		velocity.x -= (DASH_VELOCITY * BACKUP_SPEED_RATIO) * rev
 
+func setAnim(anim,rev):
+	get_node("Pounce").visible = (anim == "Pounce")
+	get_node("Bite").visible = (anim == "Bite")
+	get_node("Idle").visible = (anim == "Idle")
+
 func doInput(rev, delta):
+	cooldown -= delta
+
+	if(cooldown < 0):
+		attack_executed = ""
+		
+	if(attack_executed == "special"):
+		setAnim("Pounce",rev)
+	elif(attack_executed == "close"):
+		setAnim("Bite",rev)
+	else:
+		setAnim("Idle",rev)
+		
 	if(get_node("../").trans_time  > 0):
 		return
 	
 	time_elapsed += delta
+	
 	
 	# cancel move if holding fight keys, or in cooldown
 	if("close" in cmds or "ranged" in cmds or "special" in cmds or cooldown > 0):
@@ -96,15 +133,16 @@ func doInput(rev, delta):
 	last_inputs = cmds
 	
 
-	cooldown -= delta
 	tryAttacks(rev,delta)
+
+	
 	
 	if(charge_command == "ranged" and charge_time > RANGED_CHARGE_TWO):
-		get_node("AnimatedSprite").modulate = Color(1,0,0)
+		get_node("Idle").modulate = Color(1,0,0)
 	elif(charge_command == "ranged" and charge_time > RANGED_CHARGE_ONE):
-		get_node("AnimatedSprite").modulate = Color(1,1,0)
+		get_node("Idle").modulate = Color(1,1,0)
 	else:
-		get_node("AnimatedSprite").modulate = Color(1,1,1)
+		get_node("Idle").modulate = Color(1,1,1)
 	
 	
 	if("right" in cmds):
@@ -114,21 +152,30 @@ func doInput(rev, delta):
 
 
 func tryCloseAttack(rev, delta):
-	if(attack_executed == "close" and cooldown > CLOSE_ATTACK_LAUNCH and cooldown - delta <= CLOSE_ATTACK_LAUNCH):
-		velocity.x += CLOSE_ATTACK_VEL_BOOST * rev
-		# hit them during the pounce frame
+	if(attack_executed == "close" and cooldown > CLOSE_ATTACK_HIT and cooldown - delta <= CLOSE_ATTACK_HIT):
 		var dist = abs(global_position.x - get_node("../Player").global_position.x)
 		if(rev == 1):
 			dist = abs(global_position.x - get_node("../Enemy").global_position.x)
 		if(dist < CLOSE_ATTACK_RANGE and rev == 1):
-			get_node("../Enemy").hit(rev)
+			get_node("../Enemy").hit(rev,CLOSE_ATTACK_DAMAGE,CLOSE_ATTACK_KNOCKBACK)
 		if(dist < CLOSE_ATTACK_RANGE and rev == -1):
-			get_node("../Player").hit(rev)
+			get_node("../Player").hit(rev,CLOSE_ATTACK_DAMAGE,CLOSE_ATTACK_KNOCKBACK)
+		
+		var d = damagefx.instance()
+		get_parent().add_child(d)
+		d.global_position = global_position + Vector2(rev * 60,-5)
+		d.emitting = true
+		
+	
+	if(attack_executed == "close" and cooldown > CLOSE_ATTACK_LAUNCH and cooldown - delta <= CLOSE_ATTACK_LAUNCH):
+		velocity.x += CLOSE_ATTACK_VEL_BOOST * rev
+
 			
 	# put in an order to execute "close" attack
 	if("close" in cmds and cooldown < 0):
 		cooldown = CLOSE_ATTACK_COOLDOWN
 		attack_executed = "close"
+		get_node("Bite").frame = 0
 		charge_command = ""
 		return true
 	return false
@@ -141,7 +188,6 @@ func tryRangedAttack(rev, delta):
 		return true
 	else:
 		if(charge_command == "ranged" and charge_time > RANGED_CHARGE_ONE):
-			
 			var v = null
 	
 			if(charge_time >= RANGED_CHARGE_TWO):
@@ -156,10 +202,34 @@ func tryRangedAttack(rev, delta):
 			charge_time = 0
 			return true
 	return false
+	
+func trySpecialAttack(rev,delta):
+	if(attack_executed == "special" and cooldown > POUNCE_ATTACK_HIT and cooldown - delta <= POUNCE_ATTACK_HIT):	
+		var dist = abs(global_position.x - get_node("../Player").global_position.x)
+		if(rev == 1):
+			dist = abs(global_position.x - get_node("../Enemy").global_position.x)
+		if(dist < POUNCE_ATTACK_RANGE and rev == 1):
+			get_node("../Enemy").hit(rev, POUNCE_ATTACK_DAMAGE, POUNCE_ATTACK_KNOCKBACK)
+		if(dist < POUNCE_ATTACK_RANGE and rev == -1):
+			get_node("../Player").hit(rev, POUNCE_ATTACK_DAMAGE, POUNCE_ATTACK_KNOCKBACK)
+	
+	if(attack_executed == "special" and cooldown > POUNCE_ATTACK_LAUNCH and cooldown - delta <= POUNCE_ATTACK_LAUNCH):
+		velocity.x += POUNCE_ATTACK_VEL_BOOSTX * rev
+		velocity.y += POUNCE_ATTACK_VEL_BOOSTY
+		# hit them during the pounce frame
+
+	# put in an order to execute "close" attack
+	if("special" in cmds and cooldown < 0):
+		cooldown = POUNCE_ATTACK_COOLDOWN
+		attack_executed = "special"
+		get_node("Pounce").frame = 0
+		charge_command = ""
+		return true
+	return false
 
 func tryAttacks(rev, delta):
 	# short circuit lmoa
-	return tryCloseAttack(rev, delta) || tryRangedAttack(rev, delta)
+	return tryCloseAttack(rev, delta) || tryRangedAttack(rev, delta) || trySpecialAttack(rev,delta)
 
 func applyForces(delta):
 	velocity.x = velocity.x * (1 - (delta * DAMPEN_FACTOR))
